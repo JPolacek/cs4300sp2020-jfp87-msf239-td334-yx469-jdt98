cddc64a3ef6704993534e39660d4f694d5772d5c
Jacob Polacek <jacpolacek@gmail.com>
2020-04-08 14:50:06 -0400

    Modified index.html to reflect our team and group

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Stretches: Find a stretch to help you pain"
+net_id = "Jake Polacek:jfp87 Jonathan Tran:jdt98 Matt Frucht:msf239 Teresa Datta:td334 Yifan Xu:yx469"
 
 @irsystem.route('/', methods=['GET'])
 def search():

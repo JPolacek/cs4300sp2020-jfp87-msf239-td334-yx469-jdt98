bb87d9d91d1832b8ba1971f408fad691831ee7a5
Jacob Polacek <jacpolacek@gmail.com>
2020-04-16 16:38:32 -0400

    Improved the boolean_search function to output non-intersecting results

--- search_functions.py
+++ search_functions.py
@@ -2,7 +2,7 @@ import json
 import itertools
 
 data = {}
-with open('data/stretch.json') as f:
+with open('data/\yogajournal.json') as f:
   data = json.load(f)
 
 def boolean_search(query):
@@ -10,18 +10,66 @@ def boolean_search(query):
     Given a query, return the stretches that work on each body part
     """
     q_lower = query.lower()
-    body_parts = q_lower.split(' and ')
+    q_body_parts = q_lower.split(' and ')
     return_dict = {}
 
-    for i in range(1, len(body_parts)+1):
-        combos = list(itertools.combinations(body_parts, i))
+    for i in range(1, len(q_body_parts)+1):
+        combos = list(itertools.combinations(q_body_parts, i))
         for combo in combos:
-            l = sorted(list(combo))
-            combo_string = ', '.join(l)
+            combo_set = set(combo)
+            combo_string = ', '.join(combo_set)
             return_dict[combo_string] = []
-            for stretch in data["stretches"]:
-                body_parts_sorted = sorted(stretch["body_parts"])
-                if body_parts_sorted == l:
-                    return_dict[combo_string].append(stretch["name"])
+            for stretch in data:
+                c_body_parts = set(data[stretch]["body_part"])
+                if set(combo_set).issubset(c_body_parts):
+                    return_dict[combo_string].append(stretch)
     
-    return return_dict
+    for key in return_dict:
+        return_dict[key] = sorted(return_dict[key])
+    
+    deduped_return_dict = {}
+    for combo in list(itertools.combinations(return_dict, 2)):
+        a = combo[0]
+        b = combo[1]
+
+        if a not in b and b not in a:
+            continue
+        
+        a_pointer = 0
+        last_a = len(return_dict[a])
+        a_list = []
+
+        b_pointer = 0
+        last_b = len(return_dict[b])
+        b_list = []
+
+        while(a_pointer < last_a and b_pointer < last_b):
+            a_val = return_dict[a][a_pointer]
+            b_val = return_dict[b][b_pointer]
+
+            if a_val == b_val:
+                if a in b:
+                    b_list.append(b_val)
+                else:
+                    a_list.append(a_val)
+                a_pointer += 1
+                b_pointer += 1
+            elif a_val < b_val:
+                a_list.append(a_val)
+                a_pointer += 1
+            elif b_val < a_val:
+                b_pointer += 1
+
+        if a in deduped_return_dict:
+            temp = deduped_return_dict[a]
+            deduped_return_dict[a] = list(set(temp + a_list))
+        else:
+            deduped_return_dict[a] = a_list
+        
+        if b in deduped_return_dict:
+            temp = deduped_return_dict[b]
+            deduped_return_dict[b] = list(set(temp + b_list))
+        else:
+            deduped_return_dict[b] = b_list
+
+    return deduped_return_dict

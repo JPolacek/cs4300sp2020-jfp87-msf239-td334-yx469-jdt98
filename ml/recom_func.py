afdf166b547f8dd1a0d06c4924d5cbb781b3fac7
mclarencornell <55508484+mclarencornell@users.noreply.github.com>
2020-05-02 17:27:30 -0400

    Update recom_func.py

--- ml/recom_func.py
+++ ml/recom_func.py
@@ -2,18 +2,17 @@ import pickle
 from sklearn import preprocessing
 import numpy as np
 
-
 def get_recom(userinput):
-
+    result = []
     loadmodel = pickle.load(open('model.sav', 'rb'))
-    index_list = ['abs', 'ankles', 'arms', 'back', 'belly', 'bladder', 'brain', 'chest', 'feet', 'hamstrings', 'hands', 'heart',
-                  'hips', 'knees', 'legs', 'liver', 'lower back', 'lungs', 'neck', 'pelvis', 'shoulders', 'spine', 'thighs', 'thyroid']
+    index_list = ['abs', 'ankles', 'arms', 'back', 'belly', 'bladder', 'brain', 'chest','feet', 'hamstrings', 'hands', 'heart', 'hips', 'knees', 'legs', 'liver', 'lower back', 'lungs', 'neck', 'pelvis', 'shoulders', 'spine', 'thighs', 'thyroid']
     le = preprocessing.LabelEncoder()
     le.fit(index_list)
-    numinput = le.transform([userinput])
-    vinput = np.zeros(len(index_list)-1)
-    vinput[numinput] = 1
-    result = loadmodel.predict([vinput])[0]
+    for i in userinput:
+        numinput = le.transform([i])
+        vinput = np.zeros(len(index_list)-1)
+        vinput[numinput] = 1
+        result.append(loadmodel.predict([vinput])[0])
 
     return result
 
